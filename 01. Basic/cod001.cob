92d625c4a3583ef72a23528850bb19e09b2a98f2
elss <elissonfreelancer@gmail.com>
2025-08-30 15:41:58 -0300

    Olá mundo

---
+++ 01. Basic/cod001.cob
@@ -0,0 +1,12 @@
+      ******************************************************************
+      * DATE:2025-08-30                                                *
+      * AUTHOR: elss                                                   *
+      * VERSION: 0.0.1                                                 *
+      * DESCRIPTION: My first code. Famous "Hello Wold".               *
+      ******************************************************************
+
+       identification division.
+       program-id. helloworld.
+       procedure division.
+           display "Hello World!".
+           stop run.
